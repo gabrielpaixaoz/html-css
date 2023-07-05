d6def172b8fdf0723d5d8c0c11424bfd429a4e85
gabrielpaixaoz <121585062+gabrielpaixaoz@users.noreply.github.com>
2023-07-05 16:45:20 -0300

    ex006

---
+++ exercicios/ex026/mq005/script.js
@@ -0,0 +1,7 @@
+function clicou() {
+  if ((document.getElementById('itens').style.display == 'block')) {
+    document.getElementById('itens').style.display = 'none'
+  } else {
+    document.getElementById('itens').style.display = 'block'
+  }
+}
